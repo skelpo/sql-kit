0a735d12e8af31878149325dfe919138a565b396
Gwynne Raskind <gwynne@darkrainfall.org>
2020-01-30 22:28:14 -0600

    Add initial support for constructing and executing UPSERT queries. Currently the emitted syntax is compatible with SQLite and PostgreSQL, but not MySQL (considerable extra work is necessary to support MySQL generically, as its UPSERT support diverges radically from the standard). Some design flaws still exist, but it is possible to specify all of the common clauses needed for UPSERT. There is no support for SQLite's ON CONFLICT ROLLBACK/ABORT/FAIL/IGNORE/REPLACE syntax, nor for any of the more esoteric clauses supported by PostgreSQL; by the time these are needed, it would be unreasonable to attempt to make the query involved portable across databases.

--- Tests/SQLKitTests/SQLKitTests.swift
+++ Tests/SQLKitTests/SQLKitTests.swift
@@ -60,6 +60,29 @@ final class SQLKitTests: XCTestCase {
         try db.drop(table: "planets").ifExists().run().wait()
         XCTAssertEqual(db.results[1], "DROP TABLE `planets`")
     }
+    
+    func testUpsertWithUpdate() throws {
+        let db = TestDatabase()
+        
+        try db
+            .upsert(into: "planets")
+            .columns("id", "name", "galaxy_id", "type")
+            .values(UUID(), "Earth", UUID(), "rocky")
+            .onConflict(
+                with: ["galaxy_id", "id"],
+                where: { $0.where("galaxy_id", .notEqual, "special_galaxy_id") },
+                do: {
+                    $0.set("name", to: "another_planet")
+                      .set(excudedValueOf: "type")
+                      .where("type", .equal, "gas_giant")
+                }
+            )
+            .run().wait()
+        
+        XCTAssertEqual(db.results[0], """
+            INSERT INTO `planets` (`id`, `name`, `galaxy_id`, `type`) VALUES (?, ?, ?, ?) ON CONFLICT (`galaxy_id`, `id`) WHERE `galaxy_id` <> ? DO UPDATE  SET `name` = ?, `type` = `excluded`.`type` WHERE `type` = ?
+            """)
+    }
 }
 
 // MARK: Table Creation
